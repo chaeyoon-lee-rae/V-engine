eb9edbd2ff438b963a0233db2ca220c91de035ed
Rachel Lee <engrrachel318@gmail.com>
2026-04-10 19:17:14 +0900

    feat: create a vulkan instance

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include <memory>
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#	include <memory>
 #	include <vulkan/vulkan_raii.hpp>
+#	include <cstdlib>
+#	include <iostream>
+#	include <stdexcept>
 #else
 import vulkan;
 #endif
 #include <GLFW/glfw3.h>
-
 #include <cstdlib>
-#include <iostream>
-#include <stdexcept>
 
 const uint32_t WIDTH  = 800;
 const uint32_t HEIGHT = 600;
@@ -27,6 +27,9 @@ class HelloTriangleApplication
   private:
 	GLFWwindow *window = nullptr;
 
+	vk::raii::Context context;
+	vk::raii::Instance instance = nullptr;
+
 	void initWindow()
 	{
 		glfwInit();
@@ -39,6 +42,40 @@ class HelloTriangleApplication
 
 	void initVulkan()
 	{
+		createInstance();
+	}
+
+	void createInstance() {
+		constexpr vk::ApplicationInfo appInfo{
+			.pApplicationName = "Hello Triangle",
+			.applicationVersion = vk::makeApiVersion(0,1,0,0),
+			.pEngineName = "No Engine",
+			.engineVersion = vk::makeApiVersion(0,1,0,0),
+			.apiVersion = vk::ApiVersion14
+		};
+
+		uint32_t glfwExtensionCount = 0;
+		auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		auto extensionProperties = context.enumerateInstanceExtensionProperties();
+		for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+			if (std::ranges::none_of(extensionProperties, [glfwExtension = glfwExtensions[i]](auto const& extensionProperty) {
+				return strcmp(extensionProperty.extensionName, glfwExtension) == 0;
+				})) {
+				throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+			}
+		}
+
+		vk::InstanceCreateInfo createInfo{
+			.pApplicationInfo = &appInfo,
+			.enabledExtensionCount = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions
+		};
+
+		//for (const auto& extension : extensionProperties)
+		//	std::cout << '\t' << extension.extensionName << '\n';
+
+		instance = vk::raii::Instance(context, createInfo);
 	}
 
 	void mainLoop()
