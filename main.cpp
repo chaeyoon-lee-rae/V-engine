f892c55e6607f6b7c040ed7332458003a4ff61ab
chaeyoon-lee-rae <engrrachel318@gmail.com>
2026-05-26 20:49:17 +0900

    feat: create a staging buffer

--- main.cpp
+++ main.cpp
@@ -570,32 +570,69 @@ private:
 		throw std::runtime_error("failed to find suitable memory type!");
 	}
 
-	void createVertexBuffer()
+	std::pair<vk::raii::Buffer, vk::raii::DeviceMemory> createBuffer(vk::DeviceSize size, vk::BufferUsageFlags usage, vk::MemoryPropertyFlags properties) 
 	{
 		vk::BufferCreateInfo bufferInfo{
-			.size = sizeof(vertices[0]) * vertices.size(),
-			.usage = vk::BufferUsageFlagBits::eVertexBuffer,
+			.size = size,
+			.usage = usage,
 			.sharingMode = vk::SharingMode::eExclusive,
 		};
 
-		vertexBuffer = vk::raii::Buffer(device, bufferInfo);
+		vk::raii::Buffer buffer = vk::raii::Buffer(device, bufferInfo);
 
-		vk::MemoryRequirements memRequirements = vertexBuffer.getMemoryRequirements();
+		vk::MemoryRequirements memRequirements = buffer.getMemoryRequirements();
 
 		vk::MemoryAllocateInfo memoryAllocateInfo{
 			.allocationSize = memRequirements.size,
 			.memoryTypeIndex = findMemoryType(
 				memRequirements.memoryTypeBits,
-				vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent)
+				properties)
 		};
 
-		vetexBufferMemory = vk::raii::DeviceMemory(device, memoryAllocateInfo);
+		vk::raii::DeviceMemory bufferMemory = vk::raii::DeviceMemory(device, memoryAllocateInfo);
 
-		vertexBuffer.bindMemory(*vetexBufferMemory, 0);
+		buffer.bindMemory(*bufferMemory, 0);
 
-		void* data = vetexBufferMemory.mapMemory(0, bufferInfo.size);
-		memcpy(data, vertices.data(), bufferInfo.size);
-		vetexBufferMemory.unmapMemory();
+		return { std::move(buffer), std::move(bufferMemory) };
+	}
+
+	void copyBuffer(vk::raii::Buffer& srcBuffer, vk::raii::Buffer& dstBuffer, vk::DeviceSize size)
+	{
+		vk::CommandBufferAllocateInfo allocInfo{
+			.commandPool = commandPool,	// TODO: command pool for transfer operations? eTransient
+			.level = vk::CommandBufferLevel::ePrimary,
+			.commandBufferCount = 1,
+		};
+
+		vk::raii::CommandBuffer commandCopyBuffer = 
+			std::move(device.allocateCommandBuffers(allocInfo).front());
+
+		commandCopyBuffer.begin({ .flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit });
+		commandCopyBuffer.copyBuffer(*srcBuffer, *dstBuffer, vk::BufferCopy(0, 0, size));
+		commandCopyBuffer.end();
+
+		graphicsQueue.submit(vk::SubmitInfo{	// TODO: create a transfer queue instead
+			.commandBufferCount = 1,
+			.pCommandBuffers = &*commandCopyBuffer,}, nullptr);
+		graphicsQueue.waitIdle(); // TODO: use fence
+	}
+
+	void createVertexBuffer()
+	{
+		vk::DeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
+		auto [stagingBuffer, stagingBufferMemory] = createBuffer(bufferSize,
+			vk::BufferUsageFlagBits::eTransferSrc,
+			vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
+
+		void* dataStaging = stagingBufferMemory.mapMemory(0, bufferSize);
+		memcpy(dataStaging, vertices.data(), bufferSize);
+		stagingBufferMemory.unmapMemory();
+
+		std::tie(vertexBuffer, vetexBufferMemory) = createBuffer(bufferSize,
+			vk::BufferUsageFlagBits::eVertexBuffer | vk::BufferUsageFlagBits::eTransferDst,
+			vk::MemoryPropertyFlagBits::eDeviceLocal);
+
+		copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
 	}
 
 	// Command buffers
